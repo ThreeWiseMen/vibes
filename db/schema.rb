c99e3b7e6ae9fbe64828c441bf5022c675ea3ebc
Justin Wood <justin.k.wood@gmail.com>
2013-10-28 12:10:48 -0400

    controller for ideas with index method/view

--- db/schema.rb
+++ db/schema.rb
@@ -25,11 +25,6 @@ ActiveRecord::Schema.define(version: 20131026010244) do
     t.datetime "updated_at"
   end
 
-  create_table "user", force: true do |t|
-    t.string "ident",    limit: nil, null: false
-    t.string "password", limit: nil
-  end
-
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,10 @@
 User.delete_all
+Idea.delete_all
 
 justin = User.create({first_name: 'Justin', last_name: 'Wood',
                        email: 'woodjk@threewisemen.ca', password: 'something',
                      about_me: 'This is a small description about me'})
+
+idea = Idea.create({name: 'Something Cool', leader: 'This is the leader of my idea',
+                   description: 'This is the description of my idea. It is a really goo one',
+                   user: justin})
