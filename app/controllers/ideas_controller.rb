3354559dd4e1712ee2e232957add9c3a90714938
Justin Wood <justin.k.wood@gmail.com>
2013-10-28 13:05:48 -0400

    ability to view/create ideas

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,9 +1,22 @@
 class IdeasController < ApplicationController
+  before_filter :authenticate_user!, except: [:index, :show]
+
   def index
     @ideas = Idea.all
   end
 
   def show
     @idea = Idea.find(params[:id])
+  end
+
+  def new
+    @idea = Idea.new
+  end
+
+  def create
+    @idea = Idea.new(params.require(:idea).permit(:name, :leader, :description))
+    @idea.user = current_user
+    @idea.save
+    redirect_to idea_path @idea
   end
 end
