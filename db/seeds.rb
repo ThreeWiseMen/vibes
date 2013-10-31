6dc5239cbeaf9c497294b0bd06623caeaa476455
Justin Wood <justin.k.wood@gmail.com>
2013-10-31 16:51:44 -0400

    sign out button on the users index, show method for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,10 @@ justin = User.create({first_name: 'Justin', last_name: 'Wood',
                        email: 'woodjk@threewisemen.ca', password: 'something',
                      about_me: 'This is a small description about me'})
 
+other_user = User.create({first_name: 'Other', last_name: 'User',
+                       email: 'rawr@rawr.com', password: 'something',
+                       about_me: 'This is a small description about me'})
+
 idea = Idea.create({name: 'Something Cool', leader: 'This is the leader of my idea',
                    description: 'This is the description of my idea. It is a really goo one',
                    user: justin})
